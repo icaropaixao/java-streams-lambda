f2eb09864b0edc18cb027323d3030527ae9dd21a
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-08-28 21:00:42 -0300

    Op terminais

--- src/Streams/operacoesIntermediarias/OpIntermediarias.java
+++ src/Streams/operacoesIntermediarias/OpIntermediarias.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class OperacoesIntermediarias{
+public class OpIntermediarias {
 
     public static void main(String[] args) {
 
---
+++ src/Streams/operacoesTerminais/OpTerminais.java
@@ -0,0 +1,17 @@
+package Streams.operacoesTerminais;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OpTerminais {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 3, 5, 7, 9);
+
+        int soma = numeros.stream()
+                .filter(number -> number >= 5) // Filtrando os números maiores ou iguais a 5
+                .mapToInt(number -> number * 2) // Multiplicando cada elemento por 2
+                .reduce(0, Integer::sum); // Combinando os elementos do stream em um único resultado
+
+        System.out.println(soma); // 42
+    }
+}
