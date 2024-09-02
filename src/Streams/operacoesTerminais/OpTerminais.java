49a35ef897bd8b9d6b44586bf16d112f49f27660
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-09-02 14:29:59 -0300

    att

--- src/Streams/operacoesTerminais/OpTerminais.java
+++ src/Streams/operacoesTerminais/OpTerminais.java
@@ -2,10 +2,15 @@ package Streams.operacoesTerminais;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class OpTerminais {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 3, 5, 7, 9);
+
+        
+
 
         int soma = numeros.stream()
                 .filter(number -> number >= 5) // Filtrando os números maiores ou iguais a 5
